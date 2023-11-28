7569ce86dc65912a1fde790eacbfe1931a6af674
Lyra <lzhornyak@gmail.com>
2023-11-28 16:55:56 -0500

    change generation strategy 1

--- generate_bifurcs.py
+++ generate_bifurcs.py
@@ -4,6 +4,10 @@ import pandas as pd
 from matplotlib import pyplot as plt
 from itertools import combinations
 from string import ascii_lowercase
+import sympy as sp
+from tqdm import tqdm
+from mpire import WorkerPool
+from functools import partial
 
 from matplotlib.contour import QuadContourSet
 from scipy import ndimage
@@ -12,87 +16,30 @@ from scipy.signal import find_peaks
 from scipy.special import comb
 
 
-def generate_equations(bases, append=None):
-    par_bases = bases + [f'r * {s}' for s in bases]
-    var_sets = [s for i in range(1, len(par_bases) + 1) for s in combinations(par_bases, i)]
-    for b in bases:
-        var_sets = [v for v in var_sets if sum([b in sv for sv in v]) < 2]  # remove duplicates
-    var_sets = [v for v in var_sets if sum(['r' in sv for sv in v]) > 0]  # ensure r is present
+def generate_equations(bases, params, append=None):
+    var_sets = [s for i in range(1, len(bases) + 1) for s in combinations(bases, i)]
     equations = []
     for v in var_sets:
-        defining_pars = ascii_lowercase[:len(v)]
-        equation = ' + '.join([f'{p}*{s}' for p, s in zip(defining_pars, v)])
+        equation = ' + '.join(v)
         if append is not None:
             equation = equation + ' + ' + append
-        # equation = equation + ' - 0.01*x**5'  # ensure bounded solutions
-        equations.append((equation, defining_pars))
+        equations.append((equation, [p for p in params if p in equation]))
     return equations
 
 
-# # currently only supports 1D systems
-# def setup_system(equation, params, param_values=None):
-#     # add r to params
-#     params = 'r' + params
-#     if param_values is None:
-#         param_values = np.random.randn(len(params)).clip(-2, 2)
-#
-#     ds_args = dst.args(name='Model')
-#     ds_args.pars = {p: v for p, v in zip(params, param_values)}
-#     ds_args.varspecs = {'x': equation, 'w': 'x - w'}
-#     ds_args.ics = {'x': 0, 'w': 0}
-#     ds_args.tdomain = [0, 100]  # set the range of integration.
-#     ds_args.pdomain = {p: [-2, 2] for p in params}
-#     ds_args.xdomain = {'x': [-10, 10], 'w': [-np.inf, np.inf]}
-#     return dst.Generator.Vode_ODEsystem(ds_args)
-#
-#
-# def generate_bifurcation(ode):
-#     # set initial conditions near fixed point
-#     traj = ode.compute('trajectory')  # integrate ODE
-#     fixed = traj.sample(dt=0.1)['x'][-1]
-#     ode.set(ics={'x': fixed + 0.01})
-#
-#     # set up initial continuation class
-#     pc_args = dst.args(name='EQ1', type='EP-C')
-#     pc_args.freepars = ['r']
-#     pc_args.StepSize = 1e-3
-#     pc_args.MaxStepSize = 1e-2
-#     pc_args.MinStepSize = 1e-4
-#     pc_args.MaxNumPoints = 10000
-#     pc_args.LocBifPoints = 'all'
-#     pc_args.StopAtPoints = ['B']  # stop at boundary points
-#     pc_args.SaveEigen = True  # to tell unstable from stable branches
-#
-#     # compute bifurcation diagram
-#     pc = dst.ContClass(ode)
-#     pc.newCurve(pc_args)
-#     pc['EQ1'].forward()
-#     pc['EQ1'].backward()
-#
-#
-#     pc.display(['r', 'x'], stability=True)
-#     plt.xlim(-2, 2)
-#     plt.ylim(-10, 10)
-#     plt.show()
-
-def sample_points(equation, params, param_values=None, abs_params='', mesh_size=100):
+def sample_points(equation, params, system_var='x', bifurc_var='r', param_values=None, abs_params='', mesh_size=100):
+    r, x = sp.symbols(bifurc_var), sp.symbols(system_var)
+    params = sp.symbols(params)
+    abs_params = sp.symbols(abs_params) if abs_params else []
+    equation = sp.sympify(equation)
     if param_values is None:
-        param_values = np.random.randn(len(params)).clip(-1, 1)
+        param_values = np.random.rand(len(params)) * 2 - 1
     for p, pv in zip(params, param_values):
         if p in abs_params: pv = abs(pv)
-        equation = equation.replace(p, str(pv))
+        equation = equation.subs(p, pv)
 
-    # print(equation)
-
-    # @np.vectorize
-    def f(r, x):
-        return eval(equation)
-
+    f = sp.lambdify((r, x), equation, 'numpy')
     points = np.meshgrid(np.linspace(-1, 1, mesh_size), np.linspace(-5, 5, mesh_size))
-    # res = QuadContourSet(points[0], points[1], f(points[0], points[1]), levels=[0])
-    # plt.xlabel('r')
-    # plt.ylabel('x')
-    # plt.show()
     return equation, (points[0], points[1], f(points[0], points[1]))
 
 
@@ -140,14 +87,14 @@ def bernstein_polynomial(t, n):
 def bezier_fit(sequence, order=9):
     bounds = [sequence[0], sequence[-1]]
     if (bounds[1] - bounds[0] == 0).any():
-        return bounds, np.array([0.] * order + [1.])
+        return np.asarray(bounds).ravel(), np.array([0.] * order + [1.])
 
     unit_seq = (sequence - bounds[0]) / (bounds[1] - bounds[0])
     bern = bernstein_polynomial(unit_seq[:, 0], order)
     bern_inv = np.linalg.pinv(bern)
     params = np.array([0.] * order + [1.])  # 0 is 0 and -1 is 1
     params[1:-1] = bern_inv[1:-1] @ unit_seq[:, 1]
-    return bounds, params
+    return np.asarray(bounds).ravel(), params
 
 
 def bezier_sim(params, n_samples=100):
@@ -156,59 +103,78 @@ def bezier_sim(params, n_samples=100):
     return bern @ params
 
 
-def create_points_dataframe(segments):
-    points_data = []
-    for i, shape in enumerate(segments):
-        for j, segment in enumerate(shape):
-            for k, point in enumerate(segment):
-                points_data.append({
-                    'shape': i,
-                    'segment': j,
-                    'point': k,
-                    'r': point[0],
-                    'x': point[1]
-                })
-    dataframe = pd.DataFrame(points_data)
-    return dataframe
+def generate_data_single(*eq_info, mesh_size=1000, abs_params='', bezier_order=9):
+    r, x = sp.symbols('r, x')
+    equation, grid_points = sample_points(eq_info[0], eq_info[1], mesh_size=mesh_size, abs_params=abs_params)
+    contour_gen = contourpy.contour_generator(*grid_points, name='threaded')
+    contours = contour_gen.lines(0)
+    # prepare derivative to determine stability
+    stability = sp.lambdify((r, x), sp.diff(equation, x), 'numpy')
+    # format and resample contours
+    contour_segs = []
+    for contour in contours:
+        split_contours = split_lines(contour)
+        for sc in split_contours:
+            resample_line(sc, 100)
+            fitted_curve = bezier_fit(sc, bezier_order)
+            fitted_curve = [stability(*sc[len(sc) // 2]), *fitted_curve[0], *fitted_curve[1]]
+            contour_segs.append(fitted_curve)
+            # contour_segs.append(resample_line(sc, n_samples))
+    return equation, contour_segs
 
 
-def create_shape_dataframe(segments, equations):
-    shape_data = []
-    for i, (shape, eq) in enumerate(zip(segments, equations)):
-        shape_data.append({
-            'shape': i,
-            'n_segments': len(shape),
-            'equation': eq,
-        })
-    dataframe = pd.DataFrame(shape_data)
-    return dataframe
+def generate_data(equations, mesh_size=1000, abs_params='', bezier_order=9):
+    map_func = partial(generate_data_single, mesh_size=mesh_size, abs_params=abs_params, bezier_order=bezier_order)
+    with WorkerPool(n_jobs=12) as pool:
+        pool_data = pool.map(map_func, equations,
+                             progress_bar=True, progress_bar_options={'desc': 'Generating bifurcations'})
+    equations, raw_data = zip(*pool_data)
+    equations, raw_data = list(equations), list(raw_data)
 
+    # for i, eq in enumerate(tqdm(equations, desc='Generating data')):
+    #     # if (i + 1) % 10 == 0: print('g', i + 1)
+    #     # generate contours of bifurcation diagram
+    #     equations[i], grid_points = sample_points(eq[0], eq[1], mesh_size=mesh_size, abs_params=abs_params)
+    #     contour_gen = contourpy.contour_generator(*grid_points, name='threaded')
+    #     contours = contour_gen.lines(0)
+    #     # prepare derivative to determine stability
+    #     stability = sp.lambdify((r, x), sp.diff(sp.sympify(equations[i]), x), 'numpy')
+    #     # format and resample contours
+    #     contour_segs = []
+    #     for contour in contours:
+    #         split_contours = split_lines(contour)
+    #         for sc in split_contours:
+    #             fitted_curve = bezier_fit(sc, bezier_order)
+    #             fitted_curve = [stability(*sc[len(sc) // 2]), *fitted_curve[0], *fitted_curve[1]]
+    #             contour_segs.append(fitted_curve)
+    #             # contour_segs.append(resample_line(sc, n_samples))
+    #     raw_data.append(contour_segs)
 
-def generate_data(equations, n_samples=100, abs_params='', bezier_order=9):
-    data = []
+    index_data = np.zeros((len(raw_data), 2), dtype=int)
+    curve_idx = 0
     for i, eq in enumerate(equations):
-        if (i + 1) % 10 == 0: print('g', i + 1)
-        # generate contours of bifurcation diagram
-        equations[i], grid_points = sample_points(eq[0], eq[1], mesh_size=1000, abs_params=abs_params)
-        contour_gen = contourpy.contour_generator(*grid_points, name='threaded')
-        contours = contour_gen.lines(0)
-        # format and resample contours
-        contour_segs = []
-        for contour in contours:
-            split_contours = split_lines(contour)
-            for sc in split_contours:
-                contour_segs.append(bezier_fit(sc, bezier_order))
-                # contour_segs.append(resample_line(sc, n_samples))
-        data.append(contour_segs)
-    return data
+        index_data[i] = curve_idx, curve_idx + len(raw_data[i])
+        curve_idx += len(raw_data[i])
+    curve_data = np.concatenate(raw_data, dtype=np.float32)
+
+    return equations, index_data, curve_data
 
 
 if __name__ == '__main__':
     # equations = generate_equations(['1', 'x**1', 'x**2', 'x**3'])
-    equations = [('a*r*x - b*x**2', 'ab')] * 1000
-    data = generate_data(equations)
-    print(data)
-    points_dataframe = create_points_dataframe(data)
-    shape_dataframe = create_shape_dataframe(data, equations)
-    dataframe = points_dataframe.merge(shape_dataframe, on='shape')
-    dataframe.to_feather('data/transcritical_data.feather')
+    bases = ['a01', 'a02*r',
+             'a03*(x+b/4)', 'a04*r*(x+b/4)', 'a05*(x+b/4)**2', 'a06*r*(x+b/4)**2', 'a06*(x+b/4)**3', 'a07*r*(x+b/4)**3',
+             'a08*sin(2*c01*(x+b/4))', 'a09*r*sin(2*c02*(x+b/4))',
+             'a10*sin(2*c03*r*(x+b/4))', 'a11*r*sin(2*c04*r*(x+b/4))']
+    params = ['a01', 'a02', 'a03', 'a04', 'a05', 'a06', 'a07', 'a08', 'a09', 'a10', 'a11',
+              'b', 'c01', 'c02', 'c03', 'c04']
+    equations = generate_equations(bases, params, append='-x**5')
+    # print(len(equations))
+    # equations = [('a*r*x - b*x**2', 'a b')] * 1
+    #
+    data = generate_data(equations[:2])
+    print(len(equations))
+    # points_dataframe = create_points_dataframe(data)
+    # shape_dataframe = create_shape_dataframe(data, equations)
+    # dataframe = points_dataframe.merge(shape_dataframe, on='shape')
+    # dataframe.to_feather('data/transcritical_data.feather')
--- transformer_train.py
+++ transformer_train.py
@@ -1,7 +1,12 @@
 import torch
 from torch import nn
+import torch.nn.functional as F
+from torchvision.ops import sigmoid_focal_loss
 from torch.utils.data import Dataset, DataLoader
 import pickle
+from tqdm import tqdm
+from functools import partial
+import sympy as sp
 
 from generate_bifurcs import *
 from scipy.integrate import odeint
@@ -12,12 +17,13 @@ from bifurc_transformer import *
 
 
 def simulate(equation, n=200, n_simul=400):
-    f = eval('lambda x, t, r: ' + equation)
+    args = sp.symbols('x t r')
+    f = sp.lambdify(args, equation, 'numpy')
     x0 = np.random.rand(n) * 10 - 5
     t = np.geomspace(0.1, 10.1, n_simul) - 0.1
     r = np.random.rand(n) * 2 - 1
     x = odeint(f, x0, t, args=(r,))
-    return x.T, r
+    return np.float32(x.T), np.float32(r)
 
 
 class BifurcationPredictor(pl.LightningModule):
@@ -30,16 +36,17 @@ class BifurcationPredictor(pl.LightningModule):
         # self.emplacer = nn.Linear(1, 100)
 
         self.n_features = 400
-        pos_encoding = PositionalEncoding(self.n_features)
+        self.n_queries = 100
+
+        pos_encoding = PositionalEncoding(self.n_queries * 2)
         encoder_layer = PosEncoderLayer(pos_encoding, d_model=self.n_features, nhead=10, dim_feedforward=1000, batch_first=True)
         self.encoder = PosEncoder(encoder_layer, num_layers=4)
 
-        self.n_queries = 200
-        self.queries = nn.Embedding(self.n_queries, 400)
+        self.queries = nn.Embedding(self.n_queries, self.n_features)
         decoder_layer = PosDecoderLayer(pos_encoding, d_model=self.n_features, nhead=10, dim_feedforward=1000, batch_first=True)
         self.decoder = PosDecoder(decoder_layer, num_layers=4)
 
-        self.final_decoder = nn.Linear(self.n_features, 13)
+        self.final_decoder = nn.Linear(self.n_features, 14)
 
     def forward(self, x, r):
         # x = self.pos_encoding(x, r)
@@ -61,18 +68,21 @@ class BifurcationPredictor(pl.LightningModule):
         assignments = []
         # cost_matrices = []
         for i in range(len(x)):
-            if len(y[i][1]) > 0:
-                params = x[i, :, 3:]
+            if len(y[i]) > 0:
+                # params = x[i, :, 3:]
                 # taylor = create_taylor_vector(y[i][0][:, 0], y[i][0][:, 1], n=y[i][1].shape[1])
                 # vector = create_bezier_vector(y[i][0][:, 0], y[i][0][:, 1], n=y[i][1].shape[1])
                 # pred = (params * vector.unsqueeze(-2)).sum(-1).T  # 20 x 100
 
-                pred = x[i, :, 5:].unsqueeze(-2).expand(-1, y[i][1].shape[0], -1)
-                targ = y[i][1][:, 1:-1].unsqueeze(0).expand(x.shape[1], -1, -1)
-                cost_matrix = nn.L1Loss(reduction='none')(pred, targ).mean(dim=-1).T
-                xp = x[i, :, 1:5].unsqueeze(0).expand(y[i][0].shape[0], -1, -1)
-                yp = y[i][0].view(y[i][0].shape[0], -1).unsqueeze(1).expand(-1, x.shape[1], -1)
-                cost_matrix += nn.L1Loss(reduction='none')(xp, yp).mean(dim=-1)
+                pred = x[i, :, 6:].unsqueeze(-2).expand(-1, y[i].shape[0], -1)
+                targ = y[i][:, 6:-1].unsqueeze(0).expand(x.shape[1], -1, -1)
+                cost_matrix = F.l1_loss(pred, targ, reduction='none').mean(dim=-1).T
+                xp = x[i, :, 1:5].unsqueeze(0).expand(y[i].shape[0], -1, -1)
+                yp = y[i][:, 1:5].unsqueeze(1).expand(-1, x.shape[1], -1)
+                cost_matrix += F.l1_loss(xp, yp, reduction='none').mean(dim=-1)
+                # xs = torch.sigmoid(x[i, :, 5:6]).unsqueeze(0).expand(y[i].shape[0], -1, -1)
+                # ys = (y[i][:, 0:1] > 0).float().unsqueeze(1).expand(-1, x.shape[1], -1)
+                # cost_matrix += nn.L1Loss(reduction='none')(xs, ys).mean(dim=-1)
 
                 assignments.append(linear_sum_assignment(cost_matrix.detach().cpu().numpy()))
                 # cost_matrices.append(cost_matrix)
@@ -82,85 +92,94 @@ class BifurcationPredictor(pl.LightningModule):
         # loss for the shape
         shape_loss = []
         for i, a in enumerate(assignments):
-            params = x[i, a[1], 3:]
-            if y[i][0].numel() == 0 and y[i][1].numel() == 0:
+            # params = x[i, a[1], 3:]
+            if y[i].numel() == 0:
                 shape_loss.append(torch.tensor(0, device=x.device))
                 continue
             # taylor = create_taylor_vector(y[i][0][:, 0], y[i][0][:, 1], n=y[i][1].shape[1])
             # vector = create_bezier_vector(y[i][0][:, 0], y[i][0][:, 1], n=y[i][1].shape[1])
-            shape_loss.append(nn.L1Loss()(x[i, a[1], 5:], y[i][1][:, 1:-1]))
+            shape_loss.append(F.l1_loss(x[i, a[1], 6:], y[i][:, 6:-1]))
         shape_loss = torch.stack(shape_loss).mean()
         # shape_loss = 0
 
         # loss for the segment prediction
-        pred = x[..., 0]
-        true = torch.zeros_like(pred).bool()
+        # pred = x[..., 0]
+        # true = torch.zeros_like(pred).bool()
+        # for i, a in enumerate(assignments):
+        #     true[i, a[1]] = True
+        true = torch.zeros_like(x[..., 0])
         for i, a in enumerate(assignments):
-            true[i, a[1]] = True
-        pred_loss = (nn.BCEWithLogitsLoss()(pred[true], torch.ones_like(pred[true])) +
-                     nn.BCEWithLogitsLoss()(pred[~true], torch.zeros_like(pred[~true])))
+            true[i, a[1]] = 1
+        pred_loss = sigmoid_focal_loss(x[..., 0], true, alpha=0.25, gamma=2, reduction='mean')
+        # pred_loss = (nn.BCEWithLogitsLoss()(pred[true], torch.ones_like(pred[true])) +
+        #              nn.BCEWithLogitsLoss()(pred[~true], torch.zeros_like(pred[~true])))
 
         # loss for the position prediction
         pos_loss = []
         for i, a in enumerate(assignments):
-            if y[i][0].numel() == 0:
+            if y[i].numel() == 0:
                 pos_loss.append(torch.tensor(0, device=x.device))
                 continue
-            pos_loss.append(nn.L1Loss()(x[i, a[1], 1:5], y[i][0].view(y[i][0].shape[0], -1)))
+            pos_loss.append(nn.L1Loss()(x[i, a[1], 1:5], y[i][:, 1:5]))
         pos_loss = torch.stack(pos_loss).mean()
 
-        return shape_loss, pred_loss, pos_loss
+        # loss for the stability prediction
+        stab_loss = []
+        for i, a in enumerate(assignments):
+            if y[i].numel() == 0:
+                stab_loss.append(torch.tensor(0, device=x.device))
+                continue
+            stab_loss.append(nn.BCEWithLogitsLoss()(x[i, a[1], 5], (y[i][:, 0] > 0).float()))
+        stab_loss = torch.stack(stab_loss).mean()
+
+        return shape_loss, pred_loss, pos_loss, stab_loss
 
     def training_step(self, batch, batch_idx):
         simul, param, bifurcation = batch
 
         # random trajectory dropout
-        match np.random.rand():
-            case x if x > 0.75:
-                mask = np.random.rand(simul.shape[1]) > np.random.rand()
-                simul, param = simul[:, mask], param[:, mask]
-            case x if 0.75 > x > 0.5:
-                bounds = sorted(np.random.rand(2))
-                mask = np.logical_and(param[0, :] > bounds[0], param[0, :] < bounds[1])
-                simul, param = simul[:, mask], param[:, mask]
-            case x if 0.5 > x > 0.25:
-                bounds = sorted(np.random.rand(2))
-                mask = np.logical_and(simul[-1, :] > bounds[0], simul[-1, :] < bounds[1])
-                simul, param = simul[:, mask], param[:, mask]
+        # if self.current_epoch > 100:
+        #     match np.random.rand():
+        #         case x if x > 0.8:
+        #             mask = np.random.rand(simul.shape[1]) > np.random.rand()
+        #             simul, param = simul[:, mask], param[:, mask]
+        #         case x if 0.8 > x > 0.6:
+        #             bounds = sorted(np.random.rand(2))
+        #             mask = torch.logical_and(param[0, :] > bounds[0], param[0, :] < bounds[1])
+        #             simul, param = simul[:, mask], param[:, mask]
+            # case x if 0.5 > x > 0.25:
+            #     bounds = sorted(np.random.rand(2))
+            #     mask = torch.logical_and(simul[-1, :] > bounds[0], simul[-1, :] < bounds[1])
+            #     simul, param = simul[:, mask], param[:, mask]
 
         x = self(simul, param)
-        shape_loss, pred_loss, pos_loss = self.loss(x, bifurcation)
-        loss = 50 * shape_loss + pred_loss / 2 + 200 * pos_loss
+        shape_loss, pred_loss, pos_loss, stab_loss = self.loss(x, bifurcation)
+        loss = shape_loss + pred_loss + pos_loss + stab_loss
         self.log("shape_loss", shape_loss, prog_bar=True)
         self.log("pred_loss", pred_loss, prog_bar=True)
         self.log("pos_loss", pos_loss, prog_bar=True)
+        self.log("stab_loss", stab_loss, prog_bar=True)
         return loss
 
     def configure_optimizers(self):
-        return torch.optim.AdamW(self.parameters(), lr=1e-5, weight_decay=1e-4)
+        return torch.optim.AdamW(self.parameters(), lr=1e-4, weight_decay=1e-4)
 
 
 class BifurcationData(Dataset):
-    def __init__(self, simul_data, par_data, bifurc_data):
-        self.bifurc_data = []
-        for bd in bifurc_data:
-            bd = np.asarray(bd)
-            if len(bd) > 0:
-                bounds = torch.asarray(np.stack(bd[..., 0])).float()
-                data = torch.asarray(np.stack(bd[..., 1])).float()
-                self.bifurc_data.append((bounds, data))
-            else:
-                self.bifurc_data.append((torch.tensor([]), torch.tensor([])))
-        self.simul_data = torch.asarray(simul_data).float()
-        self.par_data = torch.asarray(par_data).float()
+    def __init__(self, simul_data, par_data, curve_index_data, curve_data):
+        self.curve_index_data = torch.asarray(curve_index_data, dtype=torch.int)
+        self.curve_data = torch.asarray(curve_data, dtype=torch.float32)
+        self.simul_data = torch.asarray(simul_data, dtype=torch.float32)
+        self.par_data = torch.asarray(par_data, dtype=torch.float32)
 
     def __len__(self):
-        return len(self.bifurc_data)
+        return len(self.curve_index_data)
 
     def __getitem__(self, idx):
+        bifurc_curves = self.curve_data[self.curve_index_data[idx][0]:self.curve_index_data[idx][1]]
         par_datum = self.par_data[idx]
         sort_mask = np.argsort(par_datum)
-        return self.simul_data[idx][sort_mask], self.par_data[idx][sort_mask], self.bifurc_data[idx]
+        return self.simul_data[idx][sort_mask], self.par_data[idx][sort_mask], bifurc_curves
 
 
 def bifurcation_collate(batch):
@@ -179,21 +198,45 @@ def create_bezier_vector(x0, x1, n=100):
 
 if __name__ == '__main__':
     try:
-        equations, dataset = pickle.load(open('dataset.pkl', 'rb'))
+        saved_data = np.load('dataset.npz')
+        data = [saved_data[key] for key in saved_data]
+    # dataset = pickle.load(open('dataset.pkl', 'rb'))
+    # dataset = pickle.load(open(r"C:\Users\Lukas\dataset.pkl", 'rb'))
     except FileNotFoundError:
-        equations = generate_equations(['1', 'x**1', 'x**2', 'x**3'], append='-x**5') * 1000
-        bifurc_data = generate_data(equations, n_samples=250, abs_params='')
-        data = []
-        for i, eq in enumerate(equations):
-            data.append(simulate(eq, n=200, n_simul=400))
-            if (i + 1) % 10 == 0: print('s', i + 1)
-        simul_data, par_data = list(zip(*data))
-        pickle.dump((equations, simul_data, par_data, bifurc_data), open('data.pkl', 'wb'))
-        data = (equations, simul_data, par_data, bifurc_data)
-        dataset = BifurcationData(*data[1:])
+        # bases = ['a01', 'a02*r',
+        #          'a03*(x+b/4)', 'a04*r*(x+b/4)', 'a05*(x+b/4)**2', 'a06*r*(x+b/4)**2', 'a06*(x+b/4)**3', 'a07*r*(x+b/4)**3',
+        #          'a08*sin(2*c01*(x+b/4))', 'a09*r*sin(2*c02*(x+b/4))',
+        #          'a10*sin(2*c03*r*(x+b/4))', 'a11*r*sin(2*c04*r*(x+b/4))']
+        bases = ['a01', 'a02*r',
+                 'a03*(x+b/2)', 'a04*r*(x+b/2)', 'a05*(x+b/2)**2', 'a06*r*(x+b/2)**2', 'a06*(x+b/2)**3', 'a07*r*(x+b/2)**3']
+        # params = ['a01', 'a02', 'a03', 'a04', 'a05', 'a06', 'a07', 'a08', 'a09', 'a10',  a11',
+        #           'b', 'c01', 'c02', 'c03', 'c04']
+        params = ['a01', 'a02', 'a03', 'a04', 'a05', 'a06', 'a07', 'b']
+        equations = generate_equations(bases, params, append='-x**5') * 100
+        print('Starting data generation')
+        equations, curve_index_data, curve_data = (
+            generate_data(equations, mesh_size=1000, abs_params='', bezier_order=9))
+        pickle.dump((equations, curve_index_data, curve_data), open('curve_data.pkl', 'wb'))
+        # curve_index_data, curve_data = pickle.load(open('curve_data.pkl', 'rb'))
 
+        map_func = partial(simulate, n=200, n_simul=100)
+        with WorkerPool(n_jobs=12) as pool:
+            data = pool.map(map_func, equations,
+                            progress_bar=True, progress_bar_options={'desc': 'Simulating systems'})
+        # for i, eq in enumerate(tqdm(equations, desc='Simulating systems')):
+        #     data.append(simulate(eq, n=200, n_simul=400))
+        #     # if (i + 1) % 10 == 0: print('s', i + 1)
+        simul_data, par_data = list(zip(*data))
+
+        data = (simul_data, par_data, curve_index_data, curve_data)
+        np.savez('dataset.npz', *data)
+        equations = [sp.pretty(eq, use_unicode=False) for eq in equations]
+        pickle.dump(equations, open('equations.pkl', 'wb'))
+
+    dataset = BifurcationData(*data)
     dataloader = DataLoader(dataset, batch_size=20, collate_fn=bifurcation_collate)
 
-    model = BifurcationPredictor()
+    # model = BifurcationPredictor()
+    model = BifurcationPredictor().load_from_checkpoint('lightning_logs/version_10/checkpoints/epoch=108-step=138975.ckpt')
     trainer = pl.Trainer(max_epochs=10000, accelerator='gpu', gradient_clip_val=0.1)
     trainer.fit(model, dataloader)
